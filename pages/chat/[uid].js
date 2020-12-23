474d7de7823bc918be041b8ccd62cf03f35560ad
Simon-Oliver <stauffer.simon@outlook.com>
2020-12-23 21:54:11 +0100

    prepareing message seen logi

--- pages/chat/[uid].js
+++ pages/chat/[uid].js
@@ -3,16 +3,23 @@ import { useRouter } from 'next/router';
 import axios from 'axios';
 import Test from '../test';
 import Pusher from 'pusher-js';
+import e from 'cors';
 
 const Chat = (props) => {
   const [response, setResponse] = useState({});
+  const [messages, setMessages] = useState([{ msg: 'Test Message', id: 123 }]);
+  const [input, setInput] = useState({ msg: '' });
   const router = useRouter();
   const { uid } = router.query;
+
+  console.log('------------------------+++++++++ Props', props);
 
   useEffect(() => {
     let pusher = new Pusher(process.env.NEXT_PUBLIC_PUSHER_APP_KEY, {
       cluster: process.env.NEXT_PUBLIC_PUSHER_APP_CLUSTER,
     });
+
+    setMessages(props.messages);
 
     let channel = pusher.subscribe('websocket-test');
 
@@ -20,11 +27,22 @@ const Chat = (props) => {
       console.log('Received users: ', data.users);
     });
 
+    channel.bind('message', function (data) {
+      setMessages(data.messages);
+      console.log('Received messages: ', data.messages);
+    });
+
+    channel.bind('messageSeen', function (data) {
+      console.log('Seen!!!!');
+    });
+
     axios.post(`http://localhost:8000/user/${uid}`, { status: 'online' });
 
     const cleanUp = () => {
       axios.post(`http://localhost:8000/user/${uid}`, { status: 'offline' });
       channel.unbind('users');
+      channel.unbind('message');
+      channel.unbind('messageSeen');
       pusher.unsubscribe('websocket-test');
       pusher.disconnect();
     };
@@ -40,13 +58,81 @@ const Chat = (props) => {
     };
   }, []);
 
+  //   useEffect(() => {
+  //     console.log('###### seen sent');
+  //     axios.post('http://localhost:8000/seen');
+  //   });
+
+  const sendMsg = (e) => {
+    e.preventDefault();
+    console.log('Sending...');
+    axios.post(`http://localhost:8000/message/${uid}`, { msg: input.msg });
+  };
+
+  const onInputChange = (e) => {
+    e.preventDefault();
+    setInput({ ...input, [e.target.name]: e.target.value });
+  };
+
+  const renderMessage = (oldMsg) => {
+    const msg = oldMsg.map((e) => {
+      if (e.id === uid) {
+        return { ...e, isUser: true };
+      } else {
+        return { ...e, isUser: false };
+      }
+    });
+
+    var previousNotUserMsg,
+      index = msg.length - 1;
+    for (; index >= 0; index--) {
+      if (msg[index].isUser == true) {
+        previousNotUserMsg = index;
+        break;
+      }
+    }
+
+    msg[previousNotUserMsg] = { ...msg[previousNotUserMsg], isLast: true };
+    console.log('<<<<<<<<<<< Render Messsage', msg);
+    return msg.map((e, i) => {
+      return (
+        <div
+          class={`${e.isUser ? 'col-start-6 col-end-13' : 'col-start-1 col-end-8'} p-3 rounded-lg`}
+        >
+          <div
+            class={`flex ${
+              !e.isUser ? 'flex-row items-center' : 'items-center justify-start flex-row-reverse'
+            }`}
+          >
+            <div class="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
+              A
+            </div>
+            <div
+              class={`relative text-sm ${
+                e.isUser ? 'bg-indigo-100 mr-3' : 'bg-white ml-3'
+              } py-2 px-4 shadow rounded-xl`}
+            >
+              <div>{e.msg}</div>
+              {e.isLast ? (
+                <div class="absolute text-xs bottom-0 right-0 -mb-5 mr-2 text-gray-500">Seen</div>
+              ) : (
+                ''
+              )}
+            </div>
+          </div>
+        </div>
+      );
+    });
+  };
+
   console.log(response);
   return (
     <div className="flex items-center justify-center">
-      <div class="rounded-lg shadow-lg border p-6 w-64 ">
-        <h5 class="text-3xl font-bold mb-4 mt-0">My Title</h5>
-        <p class="text-gray-700 text-sm pb-5">Content goes here</p>
-      </div>
+      <div class="wd-auto grid grid-cols-12 gap-y-2">{renderMessage(messages)}</div>
+      <form onSubmit={(e) => sendMsg(e)} onChange={(e) => onInputChange(e)}>
+        <input name="msg" type="text" value={input.msg}></input>
+        <button>Send MSG</button>
+      </form>
     </div>
   );
 };
--- pusher.js
+++ pusher.js
@@ -1,10 +1,8 @@
-
 const cors = require('cors');
 const uuid = require('uuid').v4;
 const next = require('next');
 const Pusher = require('pusher');
 const express = require('express');
-
 
 const dev = process.env.NODE_ENV !== 'production';
 const port = process.env.PORT || 8000;
@@ -15,96 +13,105 @@ const handler = app.getRequestHandler();
 // Ensure that your pusher credentials are properly set in the .env file
 // Using the specified variables
 const pusher = new Pusher({
-    appId: process.env.PUSHER_APP_ID,
-    key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY,
-    secret: process.env.PUSHER_APP_SECRET,
-    cluster: process.env.NEXT_PUBLIC_PUSHER_APP_CLUSTER
+  appId: process.env.PUSHER_APP_ID,
+  key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY,
+  secret: process.env.PUSHER_APP_SECRET,
+  cluster: process.env.NEXT_PUBLIC_PUSHER_APP_CLUSTER,
 });
 
-let messages = []
-let users = []
+let messages = [{ msg: 'Hi! How are you?', id: 123, isSeen: false }];
+let users = [];
 
-app.prepare()
-    .then(() => {
+app
+  .prepare()
+  .then(() => {
+    const server = express();
 
-        const server = express();
+    server.use(cors());
+    server.use(express.json());
+    server.use(express.urlencoded({ extended: true }));
 
-        server.use(cors());
-        server.use(express.json());
-        server.use(express.urlencoded({ extended: true }));
-
-        server.get('/people', (req, res, next) => {
-            res.json({ status: 'success', people: [{ name: "Test1", status: "" }, { name: "Test2", status: "" }] });
-        });
-
-        server.post('/message/:id', (req, res, next) => {
-            const id = req.params.id;
-            const { msg } = req.body
-            const time = Date.now()
-            messages.push({ msg, id, time })
-
-            console.log(req.body)
-
-            pusher.trigger('websocket-test', 'message', {
-                msg
-            });
-
-        });
-
-        server.post('/user/:id', (req, res, next) => {
-            const id = req.params.id;
-            const { name, status } = req.body
-
-
-            const index = users.findIndex((e) => e.id === id);
-
-            if (index === -1) {
-                if(name){
-                    users.push({ name, status, id });
-                }
-             
-            } else {
-                users[index] = { ...users[index], status }
-            }
-
-
-            pusher.trigger('websocket-test', 'users', {
-                users
-            });
-            console.log("After pusher trigger",users)
-        });
-
-        server.get('/init', (req, res) => {
-            let obj = {users,messages}
-            console.log(obj)
-            return res.json(obj);
-        });
-        
-
-        // server.post('/:presence/:id', (req, res, next) => {
-        //     const id = req.params.id;
-        //     const presence = req.params.presence;
-
-        //     if (['online', 'offline'].includes(presence)) {
-        //         people.forEach((person, index) => {
-        //             if (person.id === id) {
-        //                 return people[index] = { ...person, online: presence === 'online' };
-        //             }
-        //         });
-        //     }
-        // });
-
-        server.get('*', (req, res) => {
-            return handler(req, res);
-        });
-
-        server.listen(port, err => {
-            if (err) throw err;
-            console.log(`> Ready on http://localhost:${port}`);
-        });
-
-    })
-    .catch(ex => {
-        console.error(ex.stack);
-        process.exit(1);
+    server.get('/people', (req, res, next) => {
+      res.json({
+        status: 'success',
+        people: [
+          { name: 'Test1', status: '' },
+          { name: 'Test2', status: '' },
+        ],
+      });
     });
+
+    server.post('/message/:id', (req, res, next) => {
+      const id = req.params.id;
+      const { msg } = req.body;
+      const time = Date.now();
+      messages.push({ msg, id, time });
+
+      console.log(req.body);
+
+      pusher.trigger('websocket-test', 'message', {
+        messages,
+      });
+
+      return res.status('200');
+    });
+
+    server.post('/user/:id', (req, res, next) => {
+      const id = req.params.id;
+      const { name, status } = req.body;
+
+      const index = users.findIndex((e) => e.id === id);
+
+      if (index === -1) {
+        if (name) {
+          users.push({ name, status, id });
+        }
+      } else {
+        users[index] = { ...users[index], status };
+      }
+
+      pusher.trigger('websocket-test', 'users', {
+        users,
+      });
+      console.log('After pusher trigger', users);
+    });
+
+    server.post('/seen', (req, res, next) => {
+      console.log('/seen');
+      pusher.trigger('websocket-test', 'messageSeen', {
+        isSeen: true,
+      });
+    });
+
+    server.get('/init', (req, res) => {
+      let obj = { users, messages };
+      console.log(obj);
+      return res.json(obj);
+    });
+
+    // server.post('/:presence/:id', (req, res, next) => {
+    //     const id = req.params.id;
+    //     const presence = req.params.presence;
+
+    //     if (['online', 'offline'].includes(presence)) {
+    //         people.forEach((person, index) => {
+    //             if (person.id === id) {
+    //                 return people[index] = { ...person, online: presence === 'online' };
+    //             }
+    //         });
+    //     }
+    // });
+
+    server.get('*', (req, res) => {
+      return handler(req, res);
+    });
+
+    server.listen(port, (err) => {
+      if (err) throw err;
+      console.log(`> Ready on http://localhost:${port}`);
+    });
+  })
+  .catch((ex) => {
+    console.error(ex.stack);
+    process.exit(1);
+  });
