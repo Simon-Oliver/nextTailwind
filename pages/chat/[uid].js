35c9c684ce1f5dfe6d56072c04173ccb8b15c639
Simon-Oliver <stauffer.simon@outlook.com>
2020-12-23 10:47:20 +0100

    reorganising folder structure

--- pages/chat/[uid].js
+++ pages/chat/[uid].js
@@ -1,66 +1,60 @@
 import React, { useState, useEffect } from 'react';
-import { useRouter } from 'next/router'
-import axios from 'axios'
-import Test from "../test"
-import Pusher from 'pusher-js'
+import { useRouter } from 'next/router';
+import axios from 'axios';
+import Test from '../test';
+import Pusher from 'pusher-js';
 
 const Chat = (props) => {
-    const [response, setResponse] = useState({})
-    const router = useRouter()
-    const { uid } = router.query
+  const [response, setResponse] = useState({});
+  const router = useRouter();
+  const { uid } = router.query;
 
-    useEffect(() => {
-        let pusher = new Pusher(process.env.NEXT_PUBLIC_PUSHER_APP_KEY, {
-            cluster: process.env.NEXT_PUBLIC_PUSHER_APP_CLUSTER,
-        });
+  useEffect(() => {
+    let pusher = new Pusher(process.env.NEXT_PUBLIC_PUSHER_APP_KEY, {
+      cluster: process.env.NEXT_PUBLIC_PUSHER_APP_CLUSTER,
+    });
 
-        let channel = pusher.subscribe('websocket-test');
+    let channel = pusher.subscribe('websocket-test');
 
-        channel.bind('users', function (data) {
+    channel.bind('users', function (data) {
+      console.log('Received users: ', data.users);
+    });
 
-            console.log('Received users: ',data.users);
+    axios.post(`http://localhost:8000/user/${uid}`, { status: 'online' });
 
-        });
+    const cleanUp = () => {
+      axios.post(`http://localhost:8000/user/${uid}`, { status: 'offline' });
+      channel.unbind('users');
+      pusher.unsubscribe('websocket-test');
+      pusher.disconnect();
+    };
 
-        axios.post(`http://localhost:8000/user/${uid}`, { status: "online" });
+    window.onbeforeunload = () => {
+      cleanUp();
+    };
 
+    setResponse({ ...response, ...props });
 
-        const cleanUp = () => {
-            axios.post(`http://localhost:8000/user/${uid}`, { status: "offline" });
-            channel.unbind('users')
-            pusher.unsubscribe('websocket-test')
-            pusher.disconnect()
-        }
+    return () => {
+      cleanUp();
+    };
+  }, []);
 
-       
-        window.onbeforeunload = () => {
-            cleanUp()
-        }
-
-        setResponse({ ...response, ...props })
-
-        return () => {
-           cleanUp()
-        }
-    }, [])
-
-    console.log(response)
-    return (
-
-        <div className="flex items-center justify-center">
-            <div class="rounded-lg shadow-lg border p-6 w-64 ">
-                <h5 class="text-3xl font-bold mb-4 mt-0">My Title</h5>
-                <p class="text-gray-700 text-sm pb-5">Content goes here</p>
-            </div>
-        </div>
-    )
-}
+  console.log(response);
+  return (
+    <div className="flex items-center justify-center">
+      <div class="rounded-lg shadow-lg border p-6 w-64 ">
+        <h5 class="text-3xl font-bold mb-4 mt-0">My Title</h5>
+        <p class="text-gray-700 text-sm pb-5">Content goes here</p>
+      </div>
+    </div>
+  );
+};
 
 Chat.getInitialProps = async (ctx) => {
-    const res = await fetch("http://localhost:8000/init")
-    const json = await res.json()
-    return json
-}
+  const res = await fetch('http://localhost:8000/init');
+  const json = await res.json();
+  return json;
+};
 
-export default Chat
-
+export default Chat;
---
+++ pages/cv.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from 'react';
+import styles from './css.module.css';
+
+export default function Test(props) {
+  const [cv, setCv] = useState({});
+  const [formFields, setFormFields] = useState([
+    { id: 'jobTitle', type: 'text' },
+    { id: 'jobTitle', type: 'text' },
+    { id: 'jobTitle', type: 'text' },
+    { id: 'jobTitle', type: 'text' },
+    { id: 'jobTitle', type: 'text' },
+    { id: 'jobTitle', type: 'text' },
+  ]);
+
+  const getFormOptions = (e) => {
+    e.preventDefault();
+    setCv({ ...cv, [e.target.id]: e.target.value });
+  };
+
+  const addField = () => {
+    setFormFields([...formFields, { id: 'startDates', type: 'date' }]);
+  };
+
+  const renderInputfields = (fields) => {
+    return fields.map((e, index) => {
+      return (
+        <div className="rounded-md shadow-sm -space-y-px">
+          <input
+            className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+            type={e.type}
+            id={`${e.id}${index}`}
+            name={`${e.id}${index}`}
+          ></input>
+        </div>
+      );
+    });
+  };
+
+  useEffect(() => {
+    console.log(cv);
+  });
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+      <div className={`${styles.container} mt-8 space-y-6`}>
+        <form onChange={(e) => getFormOptions(e)}>{renderInputfields(formFields)}</form>
+        <button onClick={addField}>Add</button>
+      </div>
+    </div>
+  );
+}
