77340fcc21bef03b6928d29afdec408a1c7460d6
Simon-Oliver <stauffer.simon@outlook.com>
2020-12-31 09:40:26 +0100

    simple styling of users

--- pages/test.js
+++ pages/test.js
@@ -20,7 +20,21 @@ export default function Test(props) {
 
     const renderUser = () => {
         return users.map(e=> {
-            return(<div>{e.name}</div>)
+            return(<div>
+                 <button
+              class="flex flex-row items-center hover:bg-gray-100 rounded-xl p-2"
+            >
+              <div
+                class="flex items-center justify-center h-8 w-8 bg-indigo-200 rounded-full"
+              >
+                {e.name[0]}
+              </div>
+              <div class="ml-2 text-sm font-semibold">{e.name}</div>
+            </button>
+            <button
+              class="flex flex-row items-center hover:bg-gray-100 rounded-xl p-2"
+            ></button>
+            </div>)
         })
     }
 
