9dc1a119c83cd890d0b7eaf2c8e3ca4211776833
Simon-Oliver <stauffer.simon@outlook.com>
2021-02-01 21:41:55 +0100

    rendering posts

--- web/.next/static/chunks/webpack.js
+++ web/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "5a72317deb56a9a87342";
+/******/ 	var hotCurrentHash = "682020910d7cccc35adf";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
---
+++ web/pages/posts.js
@@ -0,0 +1,94 @@
+import React, { useEffect, useState } from 'react'
+import { useRouter } from 'next/router';
+import client from '../client.js'
+import BlockContent from '@sanity/block-content-to-react'
+import groq from 'groq'
+import styles from './css.module.css';
+import Link from "next/link"
+
+
+import imageUrlBuilder from '@sanity/image-url'
+
+// Get a pre-configured url-builder from your sanity client
+const builder = imageUrlBuilder(client)
+
+function urlFor(source) {
+    return builder.image(source)
+}
+
+
+
+const Post = (props) => {
+    const [imgUrl, setImgUrl] = useState("")
+
+    const router = useRouter()
+    useEffect(() => {
+        console.log(props)
+    }, [])
+
+    useEffect(() => {
+        const imgBuilder = imageUrlBuilder({
+            projectId: "6btzanu1",
+            dataset: 'production',
+        })
+        setImgUrl(imgBuilder.image(props.mainImage).width(2000).height(800))
+
+    }, [props.mainImage])
+
+
+
+    const overrides = {
+        date: props => <p className={styles.date} {...props} />,
+        h2: props => <h2 className={styles.subheading1} {...props} />,
+        h3: props => <h3 className={styles.subheading2} {...props} />,
+    }
+
+    const serializers = {
+        listItem: props => props.node.listItem == "number" ? <li className={styles.number} {...props} /> : <li className={styles.bullet} {...props} />,
+
+        types: {
+            block: props =>
+                // Check if we have an override for the “style”
+                overrides[props.node.style]
+                    // if so, call the function and pass in the children, ignoring
+                    // the other unnecessary props
+                    ? overrides[props.node.style]({ children: props.children })
+
+                    // otherwise, fallback to the provided default with all props
+                    : BlockContent.defaultSerializers.types.block(props),
+        }
+    }
+
+
+    const renderPosts = (posts) => {
+        const imgBuilder = imageUrlBuilder({
+            projectId: "6btzanu1",
+            dataset: 'production',
+        })
+        return Object.keys(posts).map((k, i) => {
+            return (
+                <>
+                    <img src={imgBuilder.image(posts[k]["mainImage"]).width(100).height(80)} />
+                    <Link href={`/post/${posts[k]["slug"].current}`}><a>{posts[k]["title"]}</a></Link>
+                </>
+            )
+        })
+    }
+
+
+    return (
+        <div className={styles.blogContainer}>
+            <p>Posts</p>
+            {renderPosts(props)}
+        </div>
+    )
+}
+
+const query = groq`*[_type == "post"]`
+
+Post.getInitialProps = async (ctx) => {
+    const { slug = "" } = ctx.query
+    return await client.fetch(query)
+}
+
+export default Post
